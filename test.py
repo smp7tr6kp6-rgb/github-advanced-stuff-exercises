57904a1845bc43d5d6d920740cde041dfbcd3d0e
Kevin Manning <kkevin13@byu.edu>
2026-09-23 16:37:32 -0600

    Add +1

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import random
 
 def generate_numbers(count, low, high):
-    return [random.randint(low, high) for _ in range(count)]
+    return [random.randint(low + 1, high) for _ in range(count)]
 
 def average(numbers):
     if not numbers:
